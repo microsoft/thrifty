b99f2b6cdfb129e410ecbdbf0aeaeda944976058
Ben Bader <ben@bendb.com>
2015-12-17 15:37:21 -0800

    Remove unused import for Checkstyle

--- thrifty-schema/src/main/java/com/bendb/thrifty/schema/Program.java
+++ thrifty-schema/src/main/java/com/bendb/thrifty/schema/Program.java
@@ -15,7 +15,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Set;
 
 /**
