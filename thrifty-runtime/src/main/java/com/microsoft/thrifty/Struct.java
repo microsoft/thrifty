10e996b2399ce783ad62c980f2f22f7157829fa5
Sean Abraham <sean.abraham@airbnb.com>
2016-07-18 12:37:40 -0700

    Checkstyle fix

--- thrifty-runtime/src/main/java/com/microsoft/thrifty/Struct.java
+++ thrifty-runtime/src/main/java/com/microsoft/thrifty/Struct.java
@@ -5,14 +5,15 @@ import com.microsoft.thrifty.protocol.Protocol;
 import java.io.IOException;
 
 /**
- * An interface that Thrift struct objects should implement
+ * An interface that Thrift struct objects should implement.
  */
 public interface Struct {
 
-    /**
-     * Writes this {@link Struct} instance to the given {@code protocol}
-     * @param protocol the protocol to which to write the struct
-     * @throws IOException if writing fails
-     */
-    void write(Protocol protocol) throws IOException;
+  /**
+   * Writes this {@link Struct} instance to the given {@code protocol}.
+   *
+   * @param protocol the protocol to which to write the struct
+   * @throws IOException if writing fails
+   */
+  void write(Protocol protocol) throws IOException;
 }
