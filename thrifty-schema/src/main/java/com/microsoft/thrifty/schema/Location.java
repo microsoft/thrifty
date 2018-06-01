8981ddc782fee8bfed1825c67cb55506a7e0aed4
Ben Bader <benjamin-bader@users.noreply.github.com>
2018-05-31 18:40:32 -0700

    Add test for circular field refs, update copyright, and fix build warnings (#169)

--- thrifty-schema/src/main/java/com/microsoft/thrifty/schema/Location.java
+++ thrifty-schema/src/main/java/com/microsoft/thrifty/schema/Location.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Microsoft Corporation
+ * Copyright (C) 2015-2018 Microsoft Corporation
  * Copyright (C) 2015 Square, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
