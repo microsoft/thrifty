ac20ca99df3f18a21c8b8ac885176ecb35d88880
Ben Bader <benjamin-bader@users.noreply.github.com>
2023-10-11 21:57:38 -0600

    Add missing license headers

--- thrifty-runtime/src/jvmMain/kotlin/com/microsoft/thrifty/transport/HttpTransport.kt
+++ thrifty-runtime/src/jvmMain/kotlin/com/microsoft/thrifty/transport/HttpTransport.kt
@@ -1,4 +1,26 @@
 /*
+ * Thrifty
+ *
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE,
+ * FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+ */
+
+// Adapted from Thrift sources; original license header follows:
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
